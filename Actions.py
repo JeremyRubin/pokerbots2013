378c30467bcbb5eed81a61a8666ab55e83dac00d
Justin Martinez <zildjian843@gmail.com>
2013-01-19 11:48:33 -0500

    Added older functions

--- Actions.py
+++ Actions.py
@@ -51,3 +51,68 @@ class Actions():
 
     def getPotSize(self):
         return self.potSize
+
+    ######### Simple Response Functions ########
+
+    # Sends action to engine
+    def return_action(action):
+        # Action should be a string, all caps, from legal actions)
+        s.send(action+"\n")
+
+    # Use this after flop analysis function to discard a card
+    def discard(card):
+        print card
+        ## Card must be a string
+        return_action('DISCARD:'+card)
+
+    # Use this to fold
+    def fold():
+        return_action('FOLD')
+
+    # Use this to call
+    def call():
+        return_action('CALL')
+
+    # Use this to check
+    def check():
+        return_action('CHECK')
+
+    # Use this to bet/raise
+    def bet_raise(amount):
+        if Player.possible_responses[0][0]:
+            return_action('BET:'+str(amount))
+        elif Player.possible_responses[5][0]:
+            return_action('RAISE:'+str(amount))
+
+    ## Very basic card discarder; always dumps the lowest card
+    def discard_low():
+        a = list(Player.holeCard1)
+        b = list(Player.holeCard2)
+        c = list(Player.holeCard3)
+        
+        value_dict = {'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'T':10,'J':11,'Q':12,'K':13,'A':14}
+        inverted_value_dict = dict([[v,k] for k,v in value_dict.items()])
+        value_array = [value_dict[a[0]] ,value_dict[b[0]],value_dict[c[0]]]
+        value_array.sort()
+        lowcard = inverted_value_dict[value_array[0]]
+        
+
+        analysis = analysis_engine.burn_which_card_simple(Player.boardCards, Player.holeCard1,Player.holeCard2,Player.holeCard3)
+
+        if analysis == 'lowcard':
+            print a[0], b[0], c[0]
+            if a[0] == lowcard:
+                discard(Player.holeCard1)
+            elif b[0] == lowcard:
+                discard(Player.holeCard2)
+            elif c[0] == lowcard:
+                discard(Player.holeCard3)
+        else:
+            choices = { 1: Player.holeCard1, 2:Player.holeCard2, 3:Player.holeCard3}
+            ar = [Player.holeCard1,Player.holeCard2,Player.holeCard3]
+            Player.burnedCard_is = ar.pop(analysis-1)
+            Player.selectedCard1 = ar.pop()
+            Player.selectedCard2 = ar.pop()
+            discard(choices[analysis])
+
+
---
+++ Betting.py
@@ -0,0 +1,8 @@
+from Actions import *
+from Cards import *
+
+
+class Betting():
+
+    def __init__(self):
+        pass
